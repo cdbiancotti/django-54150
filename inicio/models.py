4a54740e29eb1df63a8f4af78e68584221bb42f8
Cristian D Biancotti <cdbiancotti@gmail.com>
2024-06-04 21:02:18 -0300

    Clase 20 - Playground Intermedio parte 2

--- inicio/models.py
+++ inicio/models.py
@@ -3,3 +3,6 @@ from django.db import models
 class Auto(models.Model):
     modelo = models.CharField(max_length=20)
     marca = models.CharField(max_length=20)
+    
+    def __str__(self):
+        return f'Soy el auto {self.modelo} {self.marca}'
--- inicio/urls.py
+++ inicio/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from inicio import views
 
 urlpatterns = [
-    path('', views.inicio),
+    path('', views.inicio, name="inicio"),
     path('template1/<str:nombre>/<str:apellido>/<int:edad>', views.template1),
     path('template2/<str:nombre>/<str:apellido>/<int:edad>', views.template2),
     path('template3/<str:nombre>/<str:apellido>/<int:edad>', views.template3),
     path('template4/<str:nombre>/<str:apellido>/<int:edad>', views.template4),
-    path('probando/', views.probando),
-    path('autos/crear/<str:marca>/<str:modelo>/', views.crear_auto)
+    path('prueba/', views.probando, name="probando"),
+    path('autos/crear/<str:marca>/<str:modelo>/', views.crear_auto, name='crear_auto')
 ]
